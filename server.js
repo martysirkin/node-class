0a007b898c9090148a2dd8bdf1b6e50d05c71965
Marty Sirkin <martysirkin@Martys-MacBook-Pro.local>
2018-07-08 19:20:40 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} - ${req.url}`;
+
+    //console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to write to server log.');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maint.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+      pageTitle: 'Home Page',
+      welcomeMessage: 'Remember, no matter where you go - there you are!'
+  })
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+      errorMessage: 'Unable to process request.'
+  })
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000.');
+});
